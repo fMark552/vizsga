091d60ca6e2bbdcad3f886807dadef3ec96336e7
Petrik Diak <diak@petrik.hu>
2024-03-07 13:21:36 +0100

    0307.1321

--- server/controllers/Blog.js
+++ server/controllers/Blog.js
@@ -1,3 +1,7 @@
+import { db } from "../Database";
+
 export const getBlog = (req, res) => {
+  const q='SELECT blog.id, blog.text, blog.timestamp, users.username FROM blog INNER JOIN users ON blog.userId=users.id;';
+  
   res.json('Blog cucc')
 }
