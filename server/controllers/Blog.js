6364413e14a4619b26d4d092537507dcedf2a50b
Szabó Vince <vince20020622@gmail.com>
2024-03-07 16:36:25 +0100

    0307.1636

--- server/controllers/Blog.js
+++ server/controllers/Blog.js
@@ -1,7 +1,9 @@
-import { db } from "../Database";
+import { db } from '../Database.js';
 
 export const getBlog = (req, res) => {
   const q='SELECT blog.id, blog.text, blog.timestamp, users.username FROM blog INNER JOIN users ON blog.userId=users.id;';
-  
-  res.json('Blog cucc')
+  db.query(q, (err,data)=>{
+    if (err) return res.send(err);
+    return res.json(data);
+  })
 }
--- server/controllers/Comment.js
+++ server/controllers/Comment.js
@@ -1,3 +1,9 @@
+import { db } from '../Database.js';
+
 export const getComment = (req, res) => {
-  res.json('Comment cucc')
+  const q='SELECT comments.id, comments.commentText, comments.timestamp, users.username, blog.id AS blogid FROM comments INNER JOIN users ON comments.commentUserId=users.id INNER JOIN blog ON comments.commentBlogId=blog.id WHERE blogid = ?;';
+  db.query(q,[req.body.blogid],(err,data)=>{
+    if(err) return res.send(err);
+    return res.json(data);
+  })
 }
