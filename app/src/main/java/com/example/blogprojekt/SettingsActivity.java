a386b4340b1200353ec85fe891a846e3e6b53ffe
Szabó Vince <vince20020622@gmail.com>
2024-04-05 18:32:44 +0200

    0405.1832

--- app/src/main/java/com/example/blogprojekt/SettingsActivity.java
+++ app/src/main/java/com/example/blogprojekt/SettingsActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class SettingsActivity extends AppCompatActivity {
     private BottomNavigationView botnav;
+    private Button submitBtn;
+    private Date maidatum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +38,18 @@ public class SettingsActivity extends AppCompatActivity {
             }
             return false;
         });
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                maidatum= Calendar.getInstance().getTime();
+            }
+        });
     }
 
     public void init(){
         botnav=findViewById(R.id.bottom_navView);
         botnav.setSelectedItemId(R.id.menu_settings);
+        submitBtn=findViewById(R.id.submitButton);
     }
 }
